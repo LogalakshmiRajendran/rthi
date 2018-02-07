1e5f916f95c78826a9df2238321c9dc06ad39c72
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-02-07 16:24:33 +0530

    Create 56th.c

---
+++ 56th.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+int main() {
+char b[100];
+int i;
+scanf("%s",b);
+for(i=0;a[i]!='\0';i++)
+{
+  if(a[i]>='a'||a[i]<='z'||a[i]>='A'||a[i]<='Z'&&a[i]>=0||a[i]<=9)
+  {
+    printf("yes");
+    break;
+  }
+}
+}
