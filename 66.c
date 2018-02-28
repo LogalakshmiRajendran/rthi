6bab083460e9600c368c01ef145ccffd1ce83c34
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-02-28 15:47:45 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+   int a,b=0,i;
+   scanf("%d",&a);
+   for(i=2;i<a/2;i++)
+   {
+       if(a%i==0)
+       {
+           b++;
+       }
+   }
+   if(b==0)
+   printf(" prime");
+   else
+   printf("not a prime");
+}
