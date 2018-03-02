c55dd0c38d2d2c255c3c66af99a4acb375ff0bd8
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-03-02 16:45:57 +0530

    Update 56th.c

--- 56th.c
+++ 56th.c
@@ -2,10 +2,10 @@
 int main() {
 char b[100];
 int i;
-scanf("%s",b);
-for(i=0;a[i]!='\0';i++)
+scanf("%s",&b);
+for(i=0;b[i]!='\0';i++)
 {
-  if(a[i]>='a'||a[i]<='z'||a[i]>='A'||a[i]<='Z'&&a[i]>=0||a[i]<=9)
+  if(b[i]>='a'||b[i]<='z'||b[i]>='A'||b[i]<='Z'&&b[i]>=0||b[i]<=9)
   {
     printf("yes");
     break;
